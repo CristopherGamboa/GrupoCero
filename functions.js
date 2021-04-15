031e7382fcc843cc834ce49ded5e9e8903e56642
Cristopher <cristophergamboa@outlook.com>
2021-04-15 02:15:32 -0400

    v 0.3

---
+++ functions.js
@@ -0,0 +1,25 @@
+function validar() {
+    var usuario = document.getElementById("usuario").value;
+    var clave = document.getElementById("clave").value;
+
+    if (usuario == "contacto@grupocero.cl" && clave == "grupocero") {
+        alert("E-mail y contraseña válidos. ¡Bienvenido!")
+    } else {
+        alert("¡Credenciales erróneas!")
+    }
+}
+
+function buscar_obra() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('obras');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].id.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
